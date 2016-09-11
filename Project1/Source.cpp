29d4f7f22a58605ea3297e0f3af1e9055cc657ef
Rattikarn Dudley <momokojung01@hotmail.com>
2016-09-11 18:47:03 -0400

    Source code for initilize integer
    Exercise

---
+++ Project1/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// declare the legth and width variable and assign the values
+	int length = 16;
+	int width = 18;
+
+	// declare the output in perimeter variable
+	int perimeter;
+
+	//intitialize in perimeter variable
+	perimeter = 2 * (length + width);
+
+	cout << " The perimeter is  " << perimeter << endl;
+
+	system("pause");
+	return 0;
+
+}
